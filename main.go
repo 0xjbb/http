4d54feb3f159d99281c132a53fd616c94e76fe39
j <jbork12@pm.me>
2021-01-27 21:42:32 +0000

    more codes

--- main.go
+++ main.go
@@ -17,17 +17,21 @@ func main(){
 
 	port := flag.String("p", "8080", "Listening port.")
 	serDir := flag.String("d", dir, "Serve directory.")
-	upload := flag.String("u", dir + "/uploads", "Directory for uploads.")
+	//upload := flag.String("u", dir + "/uploads", "Directory for uploads.")
 	flag.Parse()
 
 	fmt.Println("[+] Listening on port", *port, "...")
 	fmt.Println("[+] Serving directory:",*serDir)
+	fmt.Println("[+] Uploads directory:")
+
 	http.HandleFunc("/upload", uploadHandler)
+
+
 	http.Handle("/", http.FileServer(http.Dir(*serDir)))
 	host := fmt.Sprintf(":%s", *port)
 	log.Fatal(http.ListenAndServe(host, nil))
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Println("test")
+	fmt.Fprintf(w,"test")
 }
